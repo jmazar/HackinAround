6d6d63a524dabe456edc26b28647e8922be4b0ad
Jeffrey Mazar <jeffrey.mazar@gmail.com>
2011-10-02 18:49:05 -0700

    Added some asm for fun.

--- HackinAround/VirtualTables/main.cpp
+++ HackinAround/VirtualTables/main.cpp
@@ -1,39 +1,62 @@
 #include "Inheritance.h"
 #include <string.h>
+#include <intrin.h>
+typedef void (*DoSomethingFunc)();
 
-typedef void (*MyFunc)();
-MyFunc funcptr;
 
-void callback()
+
+void MyFunction()
 {
-  printf("I'll just slip this right here...\n");
-  (*funcptr)();
+	printf("Inside MyFunction!\n");
 }
+
+void __cdecl printme(int a)
+{
+	printf("%d\n", a);
+}
+
 int main()
 {
-  SubClassB* b = new SubClassB();
+	//Calling from the virtual table. Gets rid of indirection
+	SubClassB* a = new SubClassB();
 
-  b->VE();
-  void** v_table = *(void***) b;
+	void** v_table = *(void***) a;
+	DoSomethingFunc myFunc = (DoSomethingFunc)(v_table[4]);
 
-  int i = 0x10 / sizeof(void*);
+	a->VE();
+	(*myFunc)();
 
-  printf("The virtual table is %d bytes!\n",i + 1);
+	int i = 0x10 / sizeof(void*);
+	printf("Size of virtual table is %d bytes.\n", i+1);
 
-  funcptr = (MyFunc)v_table[4];
+	//Stealing the virtual table for our own means
 
-  b->VE();
-  (*funcptr)();
+	void* new_vtable[5];
+	// Copy v-table 
+	memcpy(new_vtable, v_table, sizeof(new_vtable)); 
+	// Replace v-table pointer 
+	*(void **) a = new_vtable; 
 
-  void* new_vtable[5];
 
-  memcpy(new_vtable, v_table, sizeof(new_vtable));
+	new_vtable[4] = &MyFunction;
+	a->VE();
+	
+	SubClassB b;
+	b.VE();
+	__asm
+	{
+		lea ebx, a
+		mov ecx, [ebx]
 
-  *(void**) b = new_vtable;
-
-  new_vtable[4] = *callback;
-
-  b->VE();
-
-  return 0;
+		mov eax, dword ptr [a]	   //Dereferences pointer a, now pointing at the top of the object in memory
+		mov edx, dword ptr [eax]   //First four bytes a pointer to the virtual table, so dereferencing to the virtual table
+		mov eax, dword ptr [edx]   //Now at the top of the virtual table, first four bytes a pointer to the first function of the virtual table
+		call eax				   //Points to first function in the virtual table
+		mov ebx, dword ptr [b]     //Same thing as above, just don't need the first level of indirection. Moving to virtual table
+		mov eax, dword ptr [ebx]   //Going from virtual table to function
+		call eax				   //Points to first function in the virtual table
+		mov eax, dword ptr [b+0Ch] //Offset straight to virutal table. Found this by looking at disassembly and seeing it was using an offset
+		call eax				   //Points to first function in the virtual table
+	};
+	return 0;
 }
